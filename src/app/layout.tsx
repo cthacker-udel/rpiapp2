a8cc5846159b573711460bc027f479139315e8da
cthacker-udel <cthacker@udel.edu>
2024-05-23 00:50:25 -0400

    Implemented drawer for selecting pi ids

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 
 import Script from "next/script";
 import React from "react";
+import "tailwindcss/tailwind.css";
 
 import {
     bebasNeue,
@@ -10,6 +11,7 @@ import {
     quickSand,
     sourceCodePro,
 } from "@/@lib/font";
+import { ThemeProvider } from "@/@lib/themeProviderClient";
 import { QueryProvider } from "@/providers/query/QueryProvider";
 
 /**
@@ -29,7 +31,9 @@ const RootLayout = ({
         <body
             className={`${jetBrains.variable} ${quickSand.variable} ${bebasNeue.variable} ${jetBrains.variable} ${poppins.className} ${poppins.variable} ${sourceCodePro.variable} relative h-screen w-screen`}
         >
-            <QueryProvider>{children}</QueryProvider>
+            <ThemeProvider>
+                <QueryProvider>{children}</QueryProvider>
+            </ThemeProvider>
         </body>
     </html>
 );
--- src/modules/Topbar/Topbar.tsx
+++ src/modules/Topbar/Topbar.tsx
@@ -1,44 +1,77 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
+import { PiSelect } from "./PiSelect/PiSelect";
+import { Drawer } from "@material-tailwind/react";
 
 /**
  *
  * @returns
  */
-export const Topbar = (): JSX.Element => (
-    <div className="navbar bg-base-100">
-        <div className="avatar">
-            <div
-                className="w-14 rounded-full border border-black border-opacity-50 p-1 pl-2 hover:bg-gray-300 transition-all"
-                title="University of Delaware"
-            >
-                <a
-                    href="https://www.udel.edu/"
-                    rel="noreferrer"
-                    target="_blank"
+export const Topbar = (): JSX.Element => {
+    const pathName = usePathname();
+
+    const [showPiDrawer, setShowPiDrawer] = React.useState<boolean>(false);
+
+    const toggleDrawer = React.useCallback((): void => {
+        setShowPiDrawer((oldValue) => !oldValue);
+    }, []);
+
+    return (
+        <div className="navbar bg-base-100">
+            <div className="avatar">
+                <div
+                    className="w-14 rounded-full border border-black border-opacity-50 p-1 pl-2 hover:bg-gray-300 transition-all"
+                    title="University of Delaware"
                 >
-                    <Image
-                        alt="University of Delaware Logo"
-                        className="w-full"
-                        height={300}
-                        src="/udlogo.png"
-                        width={300}
-                    />
-                </a>
+                    <a
+                        href="https://www.udel.edu/"
+                        rel="noreferrer"
+                        target="_blank"
+                    >
+                        <Image
+                            alt="University of Delaware Logo"
+                            className="w-full"
+                            height={300}
+                            src="/udlogo.png"
+                            width={300}
+                        />
+                    </a>
+                </div>
+                <Link className="btn ml-3 btn-sm" href="/">
+                    {"Home"}
+                </Link>
+                <Link
+                    className={`btn ml-3 btn-sm ${pathName === "/project" ? "btn-neutral btn-disabled" : ""}`}
+                    href="/project"
+                >
+                    {"Project"}
+                </Link>
+                <Link className="btn ml-3 btn-sm" href="project/details">
+                    {"Details"}
+                </Link>
+                <Link className="btn ml-3 btn-sm" href="project/credits">
+                    {"Credits"}
+                </Link>
+                <button
+                    className="btn ml-3 btn-sm btn-info text-white"
+                    onClick={toggleDrawer}
+                    type="button"
+                >
+                    {"Select Pi"}
+                </button>
+                {/* @ts-expect-error -- ignore error for this */}
+                <Drawer
+                    className="p-4"
+                    open={showPiDrawer}
+                    overlay={false}
+                    placement="right"
+                >
+                    <div>{"Available Pavers"}</div>
+                </Drawer>
             </div>
-            <Link className="btn ml-3 btn-sm" href="/">
-                {"Home"}
-            </Link>
-            <Link className="btn ml-3 btn-sm" href="/project">
-                {"Project"}
-            </Link>
-            <Link className="btn ml-3 btn-sm" href="project/details">
-                {"Details"}
-            </Link>
-            <Link className="btn ml-3 btn-sm" href="project/credits">
-                {"Credits"}
-            </Link>
         </div>
-    </div>
-);
+    );
+};
